b68c42f7cc46fce81a7f681192ebb7c84f715ebb
Xueliang Cheng <77399327+Xue1iang@users.noreply.github.com>
2021-11-10 16:08:37 +0000

    angular name revision

--- joy2thr/src/cmd2mav.py
+++ joy2thr/src/cmd2mav.py
@@ -19,7 +19,7 @@ def cmd2mav_callback(cmd_):
     cmd_vel = OverrideRCIn()
     cmd_vel_y = 1500 + (400 * cmd_.linear.y)
     cmd_vel_x = 1500 + (400 * cmd_.linear.x)
-    cmd_vel_r = 1500 + (400 * cmd_.linear.z)
+    cmd_vel_r = 1500 + (400 * cmd_.angular.z)
     cmd_vel.channels = [0, 0, 0, cmd_vel_r, cmd_vel_x, cmd_vel_y, 0, 0]
     pub.publish(cmd_vel)
 
